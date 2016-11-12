856f1b47d6e0eb0d73aeac8a67c2b8a206e384f5
Sarah Riehl <sls.riehl@gmail.com>
2016-11-12 13:38:06 -0600

    jquery more organized

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,34 +1,51 @@
-var searchContainer = $('<div class="container container-search" style="background-color: orange; height: 6em;">');
+/* index:
+	1.  create search container
+	2.  search projects api call
+	3.  create projects submit api call
+	4.  sign up for projects submit api call
+*/
 
-$('.container-title').after(searchContainer);
 
-$('.container-search').html('<h2>search-results</h2>');
+
+//create search container
+
+var searchContainer = $('<div class="container container-search" style="background-color: orange; height: 6em;"><div class="row"><div class="col col-xs-12 col-search"></div></div></div>');
+
+$('.jumbotron').after(searchContainer);
+
+$('.col-search').html('<h2>search-results</h2>');
+
+//search for projects api call
+
 
 var currentUrl = window.location.origin;
 console.log(currentUrl);
 
 var search = $('#input-search').val();
 var response = [{
-	titleOfProject: "Paint Norma's Fence",
+	projectName: "Paint Norma's Fence",
 	location: "675 Mairo St, Austin, TX 78748",
 	description: "paint a fence for norma, she is awesome",
-	date: "08-20-2017",
-	time: "3pm",
-	role: "painter"
+	date: 20170820,
+	time: "15:00:00",
+	role: "painter",
+	duration: 4
 }, {
-	titleOfProject: "Austin Food Pantry",
+	projectName: "Austin Food Pantry",
 	location: "673 Mairo St, Austin, TX 78748",
 	description: "stock cans for the hungry",
-	date: "08-21-2017",
-	time: "10am",
-	role: "stocker"
+	date: 20170821,
+	time: "10:00:00",
+	role: "stocker",
+	duration: 2.5
 }, {
-	titleOfProject: "file papers for a professor",
+	projectName: "file papers for a professor",
 	location: "671 Mairo St, Austin, TX 78748",
 	description: "file all the newest research for a sociology professor",
-	date: "08-22-2017",
-	time: "12pm",
-	role: "filer"
+	date: 20170822,
+	time: "12:00:00",
+	role: "filer",
+	duration: 3
 
 }];
 /* $.ajax(currentUrl + '/search?' + search)
@@ -40,8 +57,11 @@ var response = [{
 		console.log(response);
 	//response should be an array with objects inside
 		for(var i = 0; i < response.length; i++) {
-			var results = $('<div><h3>' + response[i].titleOfProject + '</h3><h4>' + response[i].role + '</h4><p>' + response[i].date + ' ' + &bull; + ' ' + response[i].time + '</p><p>' + response[i].location + '</p>' + '<div>map</div><p>' + response[i].description + '</p></div>');
-			$('.container-search').append(results);
+			var results = $('<div><h3>' + response[i].titleOfProject + '</h3><h4>' + response[i].role + '</h4><p>' + response[i].date + ' &bull; ' + response[i].time + '</p><p>' + response[i].location + '</p>' + '<div>map</div><p>' + response[i].description + '</p></div>');
+			$('.col-search').append(results);
 	}
 } /* ); */
 callback(response);
+
+// create projects submit api call
+
