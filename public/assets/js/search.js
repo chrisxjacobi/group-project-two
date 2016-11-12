7b00518f07064305aa62f67162905fa83f1e5993
Sarah Riehl <sls.riehl@gmail.com>
2016-11-12 14:52:06 -0600

    add get calls to jquery

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,14 +1,14 @@
 /* index:
 	1.  create search container
-	2.  search projects api call
-	3.  create projects submit api call
-	4.  sign up for projects submit api call
+	2.  search projects api call /helpr/search?
+	3.  create projects submit api call /helpr/addproj?
+	4.  sign up for projects submit api call /helpr/help?
 */
 
 
 
 //create search container
-
+$(document).ready(function() {
 var searchContainer = $('<div class="container container-search" style="background-color: orange; height: 6em;"><div class="row"><div class="col col-xs-12 col-search"></div></div></div>');
 
 $('.jumbotron').after(searchContainer);
@@ -21,7 +21,7 @@ $('.col-search').html('<h2>search-results</h2>');
 var currentUrl = window.location.origin;
 console.log(currentUrl);
 
-var search = $('#input-search').val();
+var searchTerm = $('#input-search').val();
 var response = [{
 	projectName: "Paint Norma's Fence",
 	location: "675 Mairo St, Austin, TX 78748",
@@ -45,9 +45,9 @@ var response = [{
 	duration: 3
 
 }];
-/* $.ajax(currentUrl + '/search?' + search)
-.done( */
-	function callback(response) {
+/* $.get(currentUrl + '/helpr/search?' + searchTerm, */
+	function callback(error, response) {
+		//if(error) throw error;
 		$('.container-projects').hide();
 		$('.container-create').hide();
 		$('.container-nearby').hide();
@@ -56,9 +56,46 @@ var response = [{
 		for(var i = 0; i < response.length; i++) {
 			var results = $('<div><h3>' + response[i].titleOfProject + '</h3><h4>' + response[i].role + '</h4><p>' + response[i].date + ' &bull; ' + response[i].time + '</p><p>' + response[i].location + '</p>' + '<div>map</div><p>' + response[i].description + '</p></div>');
 			$('.col-search').append(results);
-	}
-} /* ); */
-callback(response);
+		}
+	} /* ); */
+callback(null, response);
 
 // create projects submit api call
 
+var addProject = {
+	project_name: $('#project-name').val().trim(),
+	project_date_time: $('#date-time').val().trim(), //with a function done to it to convert it to our standard format
+	project_location: $('#location').val().trim(), //maybe use google geolocator if we get there
+	project_description: $('#description').val().trim(),
+	project_role: $('#roles').val(),
+	project_duration: $('#duration').val().trim() //s/b a float
+	
+}
+
+var projectParams = $.param(addProject);
+
+$.get(currentUrl + '/helpr/addproj?' + projectParams, function(err, data) {
+	//if(err) throw err;
+	console.log(data)
+}); //get add proj
+
+//render add yourself to a project fields
+
+
+
+//add yourself to a project; volunteer
+
+var addDoer = {
+	project_name: $('#proj-volunteer-input').val(),
+	volunteer_name: $('#volunteer-name-input').val().trim(),
+	project_role: $('#roles').val()
+}
+
+var doerParams = $.param(addDoer);
+$.get(currentUrl + '/helpr/help?' + doerParams, function(err, data) {
+	//if(err) throw err;
+	console.log(data)
+}); //get add volunteer
+
+
+}); //document ready
