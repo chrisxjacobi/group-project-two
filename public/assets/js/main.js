e9e8e88e763043f7e80212ebd82fe17e05cd3477
SarahReyes <siwreyes@gmail.com>
2016-11-14 16:19:13 -0600

    changed the date-time ID name

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,5 +3,5 @@ $(document).ready(function() {
     // backstretch image in the jumbotron
     $(".jumbotron").backstretch("../public/assets/images/painting.jpg");
     // date and time picker
-    $('#datetimepicker').datetimepicker();
+    $('#date-time').datetimepicker();
 });
