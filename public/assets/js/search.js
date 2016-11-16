4c157d5fdad252075a0426083ab05edeeb0c7740
Sarah Riehl <sls.riehl@gmail.com>
2016-11-16 17:26:44 -0600

    jquery comments and more click events

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -11,71 +11,76 @@
 	var volunteerID = 1;
 
 
-//create search container
 $(document).ready(function() {
+
+	//get the current url for all api calls
 	var currentUrl = window.location.origin;
 	console.log(currentUrl);
+
+	//onclick adds a new project
 	$('#project-submit').click(function() {
-		console.log("I clicked");
-		$('#current-projects-row').hide();
-		$('#create-project-row').hide();
-		$('#projects-near-you-row').hide();
-		// create projects submit api call
+		console.log("I clicked add a new project");
+		// create projects submit api call obj
 		var addProject = {
 			project_name: $('#project-name').val().trim(),
 			project_date_time: $('#date-time').val().trim(), //with a function done to it to convert 	it to our standard format
 			project_location: $('#location').val().trim(), //maybe use google geolocator if we get 	there
-			project_description: $('#description').val().trim(),		
+			project_description: $('#description').val().trim(),
 			project_duration: $('#duration').val().trim() //s/b a float
 		};
-
+		//parameterize the addproject api call obj
 		var projectParams = $.param(addProject);
 		console.log('project params is: ' + projectParams);
-//search for projects api call and display them in the search results
+
+		//get api call to add a project through the controller, model, and orm to the sql 
 
 		$.get(currentUrl + '/helpr/addproj?' + projectParams, function callback(error, response) {
 			if(err) throw err;
 			console.log('if we"ve successfully entered a new project into sql we"ll get this non-null response is: \n\n' + response);
-		//if we have a response we'll get a modal pop up telling us we actually added a project.
+		// *** ADD MODAL *** if we have a response we'll get a modal pop up telling us we actually added a project.
 			if(response) {
 				console.log('fire modal with entry successful');
-				$('#current-projects-row').show();
-				$('#create-project-row').show();
-				$('#projects-near-you-row').show();
 			} //add proj response if
 		}); // add proj get callback
 	});// add proj project submit
 
-
+	//onclick to show all the projects in the sql
 	$('#show-all-proj').click(function() {
-
-		$('#current-projects-row').hide();
-		$('#create-project-row').hide();
-		$('#projects-near-you-row').hide();
-		
-		//*** QUESTION *** search has changed into just a display.  i don't think we need parameters but does the syntax of /helpr/search? need to change here and in the controller?
+		// get api call to get all the projects from the sql, orm, model, and controller and the callback displays them
 		$.get(currentUrl + '/helpr/search?', function callback(error, response) {
 			if(error) throw error;
 			console.log('show view all projects response: \n\n' + response + '\n\n end view all projects response');
-	//response should be an array with objects inside.  *** ADJUST *** for actual response
+			//hide the main page modules because the search results will take over
+			$('#create-project-row').hide();
+			$('#projects-near-you-row').hide();
+			//create a container to hold all the search results and a button to go back to the main view
+			var addEmAll = $('<div class="container-fluid" id="add-em-all"><div class="row"><div class="col col-xs-12" id="search-results"></div><div class="col col-xs-12"><button type="button" id="show-main">Go Back</button></div></div></div>');
+			$('.bkgrnd').after(addEmAll);
+			//response should be an array with objects inside.  *** ADJUST FOR LOOP *** for actual response
 			for(var i = 0; i < response.length; i++) {
-				var results = $('<div class="search-results"><h3>' + response[i].project_name + '</h3><p>' + response[i].project_date_time + '</p><p>' + response[i].project_location + '</p><div>map</div><p>' + response[i].project_description + '</p><button class="join-up" type="button" data-proj=' + response[i].project_id + '">Volunteer Now</button></div>');
-				$('.bkgrnd').after(results);
+				var results = $('<div class="indiv-results"><h3>' + response[i].project_name + '</h3><p>' + response[i].project_date_time + '</p><p>' + response[i].project_location + '</p><div>map</div><p>' + response[i].project_description + '</p><button class="join-up" type="button" data-proj="' + response[i].project_id + '">Volunteer Now</button></div>');
+				$('#search-results').append(results);
 			} //show all proj for
 		}); // show all proj get
 	}); //show all proj on click
-//render add yourself to a project fields in the html
 
+	//onclick to revert to the main page after viewing search results
+	$('#show-main').click(function() {
+		$('#create-project-row').show();
+		$('#projects-near-you-row').show();
+		$('#add-em-all').hide();
+	}); //show-main onclick end
 
-
-
-//send inputs to the controller to the model to the orm to add a row to the join table holding the relationships between the volunteers and the projects.
-	$('.join-up').click(function(event) {
+	//onclick join up to send inputs via a get call to the controller to the model to the orm to add a row to the join table holding the relationships between the volunteers and the projects.
+	$('.join-up').click(function(e) {
+		//create the obj to add the doer to the join table
 		var addDoer = {
-			proj_id: $(event.target).data('proj'),
+			proj_id: $(e.target).data('proj'),
 			vol_id: volunteerID 
 		};
+		//parameterize the doer obj to send it to the controller
 		var doerParams = $.param(addDoer);
+		//get call
 		$.get(currentUrl + '/helpr/help?' + doerParams, function(err, data) {
 			if(err) throw err;
 			console.log('this is the data from the joinproj callback: \n\n' + data + '\n\n end data from the joinproj callback');
